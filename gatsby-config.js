0f9a19764a58827d392e2bee169ae41878f10553
Martin kean <martinkean15@gmail.com>
2021-03-20 15:40:57 +1300

    Init

--- gatsby-config.js
+++ gatsby-config.js
@@ -6,9 +6,9 @@ require('dotenv').config({
 
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Shopify Starter`,
+    title: `Gatsby Shopify Martin Five`,
     description: `Kick off your next, ecommerce experience with this Gatsby starter. This starter ships with credentials to a shopify demo store so you can try it out immediately.`,
-    author: `@alexanderhorl`,
+    author: `@martinkean`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
